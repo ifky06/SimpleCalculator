f7125380fa72f4865f543b2d2a4d422e8c5966bd
ifky06 <ahmadrifki3403@gmail.com>
2023-01-06 11:28:59 +0700

    Add display and digits button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 function App() {
-  return <div className="App">Hello, World!!</div>;
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} type="">
+          {i}
+        </button>
+      );
+    }
+
+    return digits;
+  };
+
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>0
+        </div>
+        <div className="operators">
+          <button type="">/</button>
+          <button type="">*</button>
+          <button type="">+</button>
+          <button type="">-</button>
+
+          <button type="">DEL</button>
+
+          <div className="digits">
+            {createDigits()}
+            <button type="">0</button>
+            <button type="">.</button>
+            <button type="">=</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
